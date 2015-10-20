18c45e1fa42a94630456f72779e16c94c620df89
bengeos <george.beng@gmail.com>
2015-10-20 19:33:25 +0300

    New Draw Sine Function

--- Samples/Python/MatPlot.py
+++ Samples/Python/MatPlot.py
@@ -2,5 +2,8 @@ __author__ = 'BENGEOS-PC'
 from matplotlib import pyplot as plt
 import numpy as np
 
-def drawGrid():
-    np.arr
+def drawSine():
+    x = np.arange(-10,10,0.2)
+    plt.plot(x,np.sin(x),'r--')
+    plt.show()
+drawSine()
