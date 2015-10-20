cb11c784899c510faaa171f28601f3d22a4e160a
bengeos <george.beng@gmail.com>
2015-10-20 21:13:52 +0300

    add draw points function

--- Samples/Python/MatPlot.py
+++ Samples/Python/MatPlot.py
@@ -6,4 +6,14 @@ def drawSine():
     x = np.arange(-10,10,0.2)
     plt.plot(x,np.sin(x),'r--')
     plt.show()
-drawSine()
+
+def drawPoints():
+    x = [2,4,5.6,7,10]
+    y = [2,10,20,2,6]
+    plt.plot(x,y,'ro')
+    plt.show()
+
+
+drawPoints()
+plt.plot(np.random.normal(5,4,100),np.random.normal(10,7,100),'ro')
+plt.show()
