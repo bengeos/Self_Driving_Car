3c5fa5a49ecb0d024a6a8a599ef10ea2650207bd
bengeos <george.beng@gmail.com>
2015-10-21 22:41:45 +0300

    Test Linear Regression with Normal Equation

---
+++ Samples/Test/BEN/Linear_Regression_NormalEquation.py
@@ -0,0 +1,9 @@
+__author__ = 'BENGEOS-PC'
+import numpy as np
+from numpy import linalg as alg
+from matplotlib import pyplot as plt
+
+x = np.array([800,940,1050,1240,1300,1320,1390,1440,1450,1500,1670,1800,1950,2000,2020])
+y = np.array([170,200,220,255,290,320,350,383,400,422,450,500,540,555,570])
+
+#
