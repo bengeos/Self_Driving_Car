2af0a27c9180cb85e2aefbb5599c9696b2663455
bengeos <george.beng@gmail.com>
2015-12-18 16:13:55 -0800

    Some changes on Camera class

--- Samples/Python/Ben/Camera_Object/Camera.py
+++ Samples/Python/Ben/Camera_Object/Camera.py
@@ -7,15 +7,17 @@ class Camera(threading.Thread):
         self.Cam_Num = Cam_Num
         self.Cap = cv.VideoCapture(self.Cam_Num)
         self.Image = self.cap_img()
+        self.isRunning = True
     def cap_img(self):
         state,img = self.Cap.read()
         cv.waitKey(10)
         return img
     def run(self):
-        while(self.Cap.isOpened):
+        while(self.Cap.isOpened and self.isRunning):
             state,self.Image = self.Cap.read()
-            print("Thread",self.Image)
+            print("Threading .....")
     def get_image(self):
         cv.waitKey(10)
         return self.Image
-
+    def Stop(self):
+        self.isRunning = 0
--- Samples/Python/Ben/Camera_Object/New_Camera.py
+++ Samples/Python/Ben/Camera_Object/New_Camera.py
@@ -8,10 +8,12 @@ C1 = cm.Camera(0)
 C1.start()
 time.sleep(5)
 x = 10
-while(1):
+cond = True
+while(cond):
     x = x - 1
     time.sleep(0.5)
     print(C1.get_image())
     cv.imshow("Thread Image",C1.get_image())
+    print('>>>>>>>>>>>',x)
     if(x == 0):
-        
+        C1.Stop()
