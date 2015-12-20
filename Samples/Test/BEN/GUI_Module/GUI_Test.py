549568bead5d5b9501c14f269ab62f14b7802027
bengeos <george.beng@gmail.com>
2015-12-20 18:49:55 +0300

    added GUI Controller for SDC

---
+++ Samples/Test/BEN/GUI_Module/GUI_Test.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import serial as sp
+import time as tm
+
+cam = cv.VideoCapture(0)
+cv.namedWindow('Key Logger')
+cv.resizeWindow('Key Logger',500,500)
+Port = sp.Serial('COM4',9600)
+
+while(cam.isOpened):
+    k = cv.waitKey(5000)
+    if(k == 112):
+        print('Moving to the Right')
+        Port.write('p\r\n')
+    if(k == 111):
+        print('Moving to the Forward')
+        Port.write('o\r\n')
+    if(k == 105):
+        print('Moving to the Left')
+        Port.write('i\r\n')
+    if(k == 108):
+        print('Moving to the Backwrd')
+        Port.write('l\r\n')
+
+
